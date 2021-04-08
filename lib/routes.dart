d3bdc975c6c4ff6c2503e24a203d22e23a456268
viandwi24 <fiandwi0424@gmail.com>
2021-04-09 01:58:51 +0700

    fix

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,12 +1,14 @@
 import 'screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'screens/passenger/home/home_screen.dart';
+import 'screens/passenger/bus-list-detail/bus_list_detail_screen.dart';
 import 'screens/driver/login/login_driver.dart';
 import 'screens/test.dart';
 
 final Map<String, Widget Function(BuildContext)> routes = {
   '/': (ctx)  => HomeScreen(),
   '/passenger/home': (ctx)  => PassengerHomeScreen(),
+  '/passenger/bus-list-detail': (ctx)  => BusListDetailScreen(),
   '/driver/login': (ctx)  => LoginDriver(),
   '/test': (ctx) => TestScreen()
 };
